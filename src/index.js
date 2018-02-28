62caa05ef83a79165c429e76feccb7e42d1eb84b
mironovich-andrey <mironovich4658@gmail.com>
2018-02-28 20:47:48 +0300

    kek

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const brackets = bracketsConfig
+    .map(brackets => `${brackets[0]}${brackets[1]}`);
+  while (str.length != 0) {
+    const lengthBefore = str.length;
+    brackets.forEach(brackets => str = str.replace(brackets, ""));
+    if (lengthBefore === str.length) {
+      break;
+    }
+  };
+  return !str.length;
+};
